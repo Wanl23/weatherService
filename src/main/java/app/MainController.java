09e79a5fcd2e4802a1f49a1ae8f6e41ed54ee240
IvanLeschinsky <ivan.leschinsky@yandex.ru>
2019-07-04 03:31:55 +0500

    add saving user's data between refreshing browser

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -7,9 +7,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+@SessionAttributes("city_name")
 @Controller
 public class MainController {
+
 
     @Autowired
     private ServiceRouter serviceRouter;
@@ -21,9 +24,10 @@ public class MainController {
 
     @PostMapping("/get_forecast")
     public String getWeather(@RequestParam(name = "city_name", required = false) String city,
-                             @RequestParam(name = "service_name", required = false) String service,
-                             Model model) {
+                                   @RequestParam(name = "service_name", required = false) String service,
+                                   Model model) {
         model.addAllAttributes(serviceRouter.getForecast(city, service));
+        model.addAttribute("city_name", city);
         return "forecast";
     }
 }
